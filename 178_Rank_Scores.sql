98251bd95f854bffe9cc6ec87fc44730c13e0311
Claire Hu <claire.hu9@gmail.com>
2019-11-13 17:44:31 -0800

    Update 178_Rank_Scores.sql

--- 178_Rank_Scores.sql
+++ 178_Rank_Scores.sql
@@ -1,11 +1,23 @@
-select 
-    score,
-    dense_rank()over(order by score desc)
-from scores
+-- MS SQL
+
+-- If there is a tie between two scores, both should have the same ranking. 
+-- Note that after a tie, the next ranking number should be the next consecutive integer value.
+-- DENSE_RANK() is a rank with no gaps 
+
+SELECT 
+  Score
+  ,DENSE_RANK() over (ORDER BY Score DESC) AS Rank
+FROM scores
+ORDER BY Score DESC;
 
 
+-- MySQL
+SELECT
+  Score,
+  @rank := @rank + (@prev <> (@prev := Score)) Rank
+FROM
+  Scores,
+  (SELECT @rank := 0, @prev := -1) init
+ORDER BY Score desc
 
-/* Note: according to the question, if there is a tie between two scores, both should have the same ranking. 
-Note that after a tie, the next ranking number should be the next consecutive integer value.
-
----> DENSE_RANK() is a rank with no gaps*/
+-- https://leetcode.com/problems/rank-scores/discuss/53094/Simple-Short-Fast
