1eb4d5f7812442a5d22a9c44feb107243b667031
Claire Hu <claire.hu9@gmail.com>
2020-02-28 02:04:43 -0800

    Create 578_Get_Highest_Answer_Rate_Question.sql

---
+++ 578_Get_Highest_Answer_Rate_Question.sql
@@ -0,0 +1,11 @@
+
+
+SELECT question_id AS survey_log  
+FROM(SELECT 
+        question_id
+        ,SUM(CASE WHEN action = 'answer' THEN 1 ELSE 0 END) AS answer_num
+        ,SUM(CASE WHEN action = 'show' THEN 1 ELSE 0 END) AS show_num
+     FROM survey_log
+     GROUP BY question_id) AS t
+WHERE answer_num/show_num =1
+
