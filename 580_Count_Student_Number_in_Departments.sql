d4f0ecb5bfa530c873a9369e9ac9351221af1cd8
Claire Hu <claire.hu9@gmail.com>
2020-03-02 10:44:36 -0800

    Update 580_Count_Student_Number_in_Departments.sql

--- 580_Count_Student_Number_in_Departments.sql
+++ 580_Count_Student_Number_in_Departments.sql
@@ -1,6 +1,8 @@
 /*
+join before aggreateing:
 NO need to handle null if tables are joined before aggregating.
 NULL columns are simply not counted (return 0 if departmend has no student)
+aggreate before join:
 If studnets are counted before joining, only existing department
 with at least 1 student are aggregated over with count. 
 Left joining full dept tables (including empty dept) will result in NULL */
