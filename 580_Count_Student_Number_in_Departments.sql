7c3437383eefbba0a9a6585ce077916a137ff6cc
Claire Hu <claire.hu9@gmail.com>
2020-03-02 11:02:22 -0800

    Update 580_Count_Student_Number_in_Departments.sql

--- 580_Count_Student_Number_in_Departments.sql
+++ 580_Count_Student_Number_in_Departments.sql
@@ -19,3 +19,31 @@ LEFT JOIN
  GROUP BY dept_id) as t
 ON d.dept_id  = t.dept_id 
  
+-- To fix this, use IFNULL to display the department name with 0 student. Need a AS after IFNULL.
+-- Answer 1: 
+ SELECT 
+    d.dept_name
+    ,ifnull(t.student_number,0)as student_number
+FROM department d
+LEFT JOIN
+ (SELECT dept_id, count(student_id) as student_number 
+ FROM student 
+ GROUP BY dept_id) as t
+ON d.dept_id  = t.dept_id 
+ORDER BY student_number DESC, d.dept_name ASC;
+
+
+-- Answer 2: join before aggreateing
+
+SELECT 
+    d.dept_name
+    ,COUNT(s.student_id) AS student_number
+FROM 
+    department d
+LEFT JOIN
+    student s
+ON d.dept_id = s.dept_id
+GROUP BY d.dept_name                               -- group by department_name
+ORDER BY student_number DESC, d.dept_name ASC;
+
+
