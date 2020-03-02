56efcc391cf1147071f2356f7f5410c56448fe50
Claire Hu <claire.hu9@gmail.com>
2020-03-02 10:06:55 -0800

    Update 570_Managers_with_at_Least_5_Direct Reports.sql

--- 570_Managers_with_at_Least_5_Direct Reports.sql
+++ 570_Managers_with_at_Least_5_Direct Reports.sql
@@ -1,3 +1,4 @@
+-- Solution 1
 SELECT 
     Name   
 FROM 
@@ -6,3 +7,13 @@ WHERE ID IN(
     SELECT Managerid FROM Employee m
      GROUP BY m.ManagerId 
       HAVING COUNT(m.ManagerId)>=5)
+
+-- Solution 2
+SELECT   
+  m.name
+FROM Employee e
+JOIN Employee  m
+ON e.ManagerId = m.id
+GROUP BY e.ManagerId
+HAVING COUNT(*)>=5
+
