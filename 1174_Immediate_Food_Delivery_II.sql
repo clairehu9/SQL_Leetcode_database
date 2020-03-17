52d7f2155cb695fa2b948d135a5abb19f9251385
Claire Hu <claire.hu9@gmail.com>
2020-03-17 16:45:16 -0700

    Update 1174_Immediate_Food_Delivery_II.sql

--- 1174_Immediate_Food_Delivery_II.sql
+++ 1174_Immediate_Food_Delivery_II.sql
@@ -3,11 +3,11 @@ select top 1 with ties delivery_id, customer_id, order_date, customer_pref_deliv
 from Delivery order by rank() over (partition by customer_id order by order_date)
 )
 
-select 
-round(100*sum(case when order_date = customer_pref_delivery_date then 1 else 0 end)/
-count(distinct customer_id), 2) as immediate_percentage from first;
+select round(100 * sum(case when order_date = customer_pref_delivery_date then 1.0 else 0.0 end)/    
+             count(distinct customer_id), 2) as immediate_percentage from first;
 
 
+-- then 1 else 0 end don't work on leetcode
 #####################################################################################################
 
 
