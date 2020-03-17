5a9e419fa0bcbc758714d86e9d963f2f9a317f1d
Claire Hu <claire.hu9@gmail.com>
2020-03-17 00:13:58 -0700

    Create 1126_Active_Businesses.sql

---
+++ 1126_Active_Businesses.sql
@@ -0,0 +1,46 @@
+-- sql
+
+SELECT business_id
+FROM Events e
+LEFT JOIN
+    (SELECT event_type, avg(occurences) AS avg_occ
+    FROM Events 
+    GROUP BY event_type) AS sub
+ON e.event_type = sub.event_type 
+WHERE e.occurences > sub.avg_occ 
+GROUP BY business_id 
+HAVING COUNT(*)>1
+   
+   
+   
+-- window function
+
+with tmp as (
+select business_id, occurences, avg(occurences)over(partition by event_type) as average
+from events
+)
+
+select business_id
+from tmp
+where occurences > average
+group by business_id  
+having count(*)>1
+
+
+/*
+select business_id, occurences, avg(occurences)over(partition by event_type) as average from events
+
+output:
+{"headers": ["business_id", "occurences", "average"], "values": [
+[1, 11, 8], 
+[2, 7, 8], 
+[3, 6, 8], 
+[1, 3, 7], 
+[2, 12, 7],  
+[1, 7, 5], 
+[3, 3, 5]]}
+*/
+
+
+
+
