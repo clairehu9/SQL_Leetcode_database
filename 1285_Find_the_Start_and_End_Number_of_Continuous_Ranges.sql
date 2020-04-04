39badecc84e217548839edbbc0bb4d061faec54d
Claire Hu <claire.hu9@gmail.com>
2020-04-04 01:47:20 -0700

    Update 1285_Find_the_Start_and_End_Number_of_Continuous_Ranges.sql

--- 1285_Find_the_Start_and_End_Number_of_Continuous_Ranges.sql
+++ 1285_Find_the_Start_and_End_Number_of_Continuous_Ranges.sql
@@ -1,14 +1,25 @@
 WITH tmp AS
 (
 SELECT log_id
-    ,log_id - ROW_NUMBER() OVER(ORDER BY log_id) AS rnk
+    ,log_id - ROW_NUMBER() OVER(ORDER BY log_id) AS difference
 FROM Logs
 )
 
 SELECT MIN(log_id) AS start_id
 ,MAX(log_id) AS end_id
 FROM tmp
-GROUP BY rnk
+GROUP BY difference
 ORDER BY start_id
 
 
+/*
+
+SELECT log_id
+    ,ROW_NUMBER() OVER(ORDER BY log_id) AS rnk
+    ,(log_id - ROW_NUMBER() OVER(ORDER BY log_id)) AS difference 
+FROM Logs
+
+["log_id", "rnk", "difference"]
+[1, 1, 0], [2, 2, 0], [3, 3, 0], [7, 4, 3], [8, 5, 3], [10, 6, 4]]}
+
+*/
