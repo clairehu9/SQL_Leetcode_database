f543dff9477199b32a953558579510d72ff0f6d3
Claire Hu <claire.hu9@gmail.com>
2020-04-04 01:20:09 -0700

    Create 1270._All_People_Report_to_the_Given_Manager.sql

---
+++ 1270._All_People_Report_to_the_Given_Manager.sql
@@ -0,0 +1,50 @@
+select e1.employee_id 
+from Employees e1
+join Employees e2
+  on e2.employee_id = e1.manager_id  
+join Employees e3
+  on e3.employee_id=e2.manager_id 
+WHERE e3.manager_id = 1 AND e1.employee_id != 1
+
+/* 
+
+1 is the boss
+4 --> 2 --> 1,
+7 --> 4 --> 2 --> 1
+
+
+select *
+from Employees e1
+join Employees e2
+  on e2.employee_id = e1.manager_id 
+
+{"headers": ["employee_id", "employee_name", "manager_id", "employee_id", "employee_name", "manager_id"], "values": [
+[1, "Boss", 1, 1, "Boss", 1], 
+[2, "Bob", 1, 1, "Boss", 1], 
+[77, "Robert", 1, 1, "Boss", 1], 
+[3, "Alice", 3, 3, "Alice", 3], 
+[8, "John", 3, 3, "Alice", 3], 
+[4, "Daniel", 2, 2, "Bob", 1], 
+[7, "Luis", 4, 4, "Daniel", 2], 
+[9, "Angela", 8, 8, "John", 3]]}
+
+
+
+select *
+from Employees e1
+join Employees e2
+  on e2.employee_id = e1.manager_id 
+join Employees e3
+on e3.employee_id=e2.manager_id 
+
+{"headers": ["employee_id", "employee_name", "manager_id", "employee_id", "employee_name", "manager_id", "employee_id", "employee_name", "manager_id"], "values": [
+[1, "Boss", 1, 1, "Boss", 1, 1, "Boss", 1],           Boss 
+[2, "Bob", 1, 1, "Boss", 1, 1, "Boss", 1],            2 -> 1
+[77, "Robert", 1, 1, "Boss", 1, 1, "Boss", 1],        77-> 1
+[4, "Daniel", 2, 2, "Bob", 1, 1, "Boss", 1],          4-> 2 ->1
+[3, "Alice", 3, 3, "Alice", 3, 3, "Alice", 3], 
+[8, "John", 3, 3, "Alice", 3, 3, "Alice", 3], 
+[9, "Angela", 8, 8, "John", 3, 3, "Alice", 3],     
+[7, "Luis", 4, 4, "Daniel", 2, 2, "Bob", 1]]}         7 -> 4 -> 2 -> 1
+
+*/
