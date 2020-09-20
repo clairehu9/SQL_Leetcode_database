a8a835326fd53d2ffc6b76950678e28dfdcf8eff
Claire Hu <claire.hu9@gmail.com>
2020-09-20 00:22:38 -0700

    Create 511_Game_Play_Analysis_I.sql

---
+++ 511_Game_Play_Analysis_I.sql
@@ -0,0 +1,12 @@
+select distinct player_id,
+first_value(event_date)over(partition by player_id order by event_date asc) as first_login 
+from activity
+
+*Note:要加distinct
+如果不加distinct，出来的结果是：每一个球员id对应的都是最早日期
+ ["player_id", "first_login"], 
+ [[1, "2016-03-01"], 
+ [1, "2016-03-01"], 
+ [2, "2017-06-25"], 
+ [3, "2016-03-02"], 
+ [3, "2016-03-02"]]}
