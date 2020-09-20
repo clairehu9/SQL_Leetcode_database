03b55ac042d97e0c85b7d1c562434940bc3ffc9f
Claire Hu <claire.hu9@gmail.com>
2020-09-20 00:26:19 -0700

    Update 511_Game_Play_Analysis_I.sql

--- 511_Game_Play_Analysis_I.sql
+++ 511_Game_Play_Analysis_I.sql
@@ -1,8 +1,10 @@
+/*window function */
+
 select distinct player_id,
 first_value(event_date)over(partition by player_id order by event_date asc) as first_login 
 from activity
 
-*Note:要加distinct
+/*Note:要加distinct
 如果不加distinct，出来的结果是：每一个球员id对应的都是最早日期
  ["player_id", "first_login"], 
  [[1, "2016-03-01"], 
@@ -10,3 +12,9 @@ from activity
  [2, "2017-06-25"], 
  [3, "2016-03-02"], 
  [3, "2016-03-02"]]}
+ */
+
+/* MySQL */
+select distinct player_id, min(event_date) as first_login 
+from activity 
+group by player_id
