f5f1d72f7898f2aa04916f3f5a6be9b289fcc9e2
Claire Hu <claire.hu9@gmail.com>
2020-09-20 13:53:04 -0700

    Update 512_Game_Play_Analysis_II.sql

--- 512_Game_Play_Analysis_II.sql
+++ 512_Game_Play_Analysis_II.sql
@@ -29,3 +29,14 @@ select player_id, device_id
 from activity 
 where (player_id, event_date) in (select player_id, min(event_date) from activity group by player_id)
 
+
+
+
+
+with cte as
+(select player_id,
+        device_id,
+        rank()over(partition by player_id order by event_date) as rnk
+from activity)
+
+select player_id, device_id from cte where rnk = 1
