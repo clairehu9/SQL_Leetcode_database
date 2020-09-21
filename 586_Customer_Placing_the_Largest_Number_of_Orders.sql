c7287fe7c22856914dd9d4b7bbda4aa2a22cb9f1
Claire Hu <claire.hu9@gmail.com>
2020-09-21 00:14:40 -0700

    Update 586_Customer_Placing_the_Largest_Number_of_Orders.sql

--- 586_Customer_Placing_the_Largest_Number_of_Orders.sql
+++ 586_Customer_Placing_the_Largest_Number_of_Orders.sql
@@ -8,3 +8,15 @@ HAVING COUNT(order_number) = (
 	ORDER BY cnt DESC
 	LIMIT 1
 )
+
+/* wrong
+SELECT customer_number
+FROM orders
+WHERE order_number = (SELECT COUNT(order_number) cnt
+	                    FROM orders
+	                    GROUP BY customer_number
+	                    ORDER BY cnt DESC
+                        LIMIT 1)
+			
+			
+ */
