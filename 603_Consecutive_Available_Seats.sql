4198c0ef6ff631c9c15757a684092b64f78e5013
Claire Hu <claire.hu9@gmail.com>
2020-09-21 14:18:06 -0700

    Update 603_Consecutive_Available_Seats.sql

--- 603_Consecutive_Available_Seats.sql
+++ 603_Consecutive_Available_Seats.sql
@@ -1,4 +1,14 @@
-/* Window function */
+/* Window function 
+
+seat_id	free	free_lag	free_lead
+1	1	NULL	0
+2	0	1	1
+3	1	0	1
+4	1	1	1
+5	1	1	NULL
+(free=1 AND free_lag=1) can choose consecutive seats without first one
+(free=1 AND free_lead=1) can choose consecutive seats without last one
+Use OR to combine these two to get correct result. */
 
 with cte as(
 select 
@@ -13,3 +23,4 @@ select seat_id
 from cte
 where (free=1 and free_lag=1)
 or (free=1 and free_lead=1)
+
