9d0d05ebee2c06625f98b9662665ee1014009988
Claire Hu <claire.hu9@gmail.com>
2020-09-21 23:47:57 -0700

    Create 1113_Reported_Posts.sql

---
+++ 1113_Reported_Posts.sql
@@ -0,0 +1,13 @@
+/*Do not 画蛇添足:
+
+No need to filter extra is not null. When action = 'report', extra is guaranteed not null.
+No need to filter count > 0. If a group exists, it must have at least one row.
+-- shawlu*/
+
+select 
+  extra as report_reason
+  ,count(distinct post_id) as report_count
+from Actions
+where action_date = '2019-07-04'
+  and action = 'report'
+group by extra
