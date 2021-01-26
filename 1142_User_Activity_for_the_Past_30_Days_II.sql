d158a880f9381efe4312ba321b2779801056ff81
Claire Hu <claire.hu9@gmail.com>
2021-01-25 22:46:45 -0800

    Update 1142_User_Activity_for_the_Past_30_Days_II.sql

--- 1142_User_Activity_for_the_Past_30_Days_II.sql
+++ 1142_User_Activity_for_the_Past_30_Days_II.sql
@@ -4,3 +4,17 @@ from Activity
 where activity_date between '2019-06-28' and '2019-07-27'
 group by user_id 
 ) a;
+
+
+-- 2 
+
+select ifnull(round(avg(sess_count), 2) ,0) as average_sessions_per_user
+from (
+select user_id
+        ,count(distinct session_id) as sess_count
+from activity
+where activity_date between date_sub('2019-07-27',interval 29 day) 
+and '2019-07-27'
+group by user_id) as t
+
+
