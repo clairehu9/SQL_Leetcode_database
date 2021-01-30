a34a66a2d6039733926df152a7da8d1cece0c71a
Claire Hu <claire.hu9@gmail.com>
2021-01-29 16:08:26 -0800

    Update 197_Rising_Temperature.sql

--- 197_Rising_Temperature.sql
+++ 197_Rising_Temperature.sql
@@ -3,3 +3,11 @@ from weather w1
 inner join weather w2
 on datediff(w1.recorddate, w2.recorddate) = 1
 
+SELECT
+    weather.id AS 'Id'
+FROM
+    weather
+        JOIN
+    weather w ON DATEDIFF(weather.recordDate, w.recordDate) = 1
+        AND weather.Temperature > w.Temperature
+;
