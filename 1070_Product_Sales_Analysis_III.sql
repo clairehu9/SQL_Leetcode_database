d076cbc1b153acdbf41a26eab93579d5ca063fa8
Claire Hu <claire.hu9@gmail.com>
2021-07-27 21:58:59 -0700

    Update 1070_Product_Sales_Analysis_III.sql

--- 1070_Product_Sales_Analysis_III.sql
+++ 1070_Product_Sales_Analysis_III.sql
@@ -13,3 +13,12 @@ WHERE rnk =1
 select top 1 with ties product_id, year as first_year, quantity, price
 from Sales
 order by rank() over (partition by product_id order by year);
+
+
+-- mysql
+SELECT product_id, year AS first_year, quantity, price
+FROM Sales
+WHERE (product_id, year) IN (
+SELECT product_id, MIN(year) as year
+FROM Sales
+GROUP BY product_id) ;
